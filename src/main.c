c0e38a180b88177e2bca20c9fb399a704b112541
Evan Kahn <ekahn@hmc.edu>
2017-07-25 14:18:22 -0700

    Removed dependence on custom library for deep sleep.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "mgos.h"
 #include <stdio.h>
 #include "mjs.h"
+#include <esp_deep_sleep.h>
 
 enum mgos_app_init_result mgos_app_init(void) {
   return MGOS_APP_INIT_SUCCESS;
@@ -8,4 +9,9 @@ enum mgos_app_init_result mgos_app_init(void) {
 
 void print_from_c(void){
   printf("Hello from C\n");
+}
+
+void esp_deep_sleep_ext0(int pin, int level) {
+  esp_deep_sleep_enable_ext0_wakeup(pin, level);
+  esp_deep_sleep_start();
 }
