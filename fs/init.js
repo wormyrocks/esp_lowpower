54c40536dd50402f894e4f6a99ca4edaf7de69e2
Evan Kahn <ekahn@hmc.edu>
2017-07-26 12:12:31 -0700

    added comments, cleaned things up a bit

--- fs/init.js
+++ fs/init.js
@@ -3,23 +3,30 @@ load('api_timer.js');
 load('api_lowpower.js');
 
 let led=17;
-let print_from_c = ffi('void print_from_c(void)');
 let timer_id=0;
 let button=0;
 
 // If we have just woken up from deep sleep, make sure to configure pin 0
 // as GPIO rather than RTC
 
+// this doesn't work yet!!
+
 // initDeepSleep(button);
+
+// initialize touchpad input
 lowpower.initTouchpad();
 
-// Blink built-in LED every second
+// Blink LED connected to GPIO17 every second
 GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 GPIO.set_mode(button, GPIO.MODE_INPUT);
 
-Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
+Timer.set(1000, true, function() {
+  // blinky
   let value = GPIO.toggle(led);
-  print(lowpower.readTouchpad(4));
+
+  // use this for touchpad threshold calibration
+  // TOUCHPAD_NUM0 = GPIO4, btw
+  print(lowpower.readTouchpad(0));
 }, null);
 
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
@@ -28,8 +35,15 @@ GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function()
     Timer.del(timer_id);
   }
   timer_id = Timer.set(5000, false, function() {
-    print("I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion.\nI watched C-beams glitter in the dark near the Tannhauser Gate.\nAll those moments will be lost in time, like tears in rain.\nTime to die.\n");
+    print("I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion.");
+    print("I watched C-beams glitter in the dark near the Tannhauser Gate.");
+    print("All those moments will be lost in time, like tears in rain.");
+    print("Time to die.");
+
+    // wake on touch
     lowpower.wakeOnTouch(4);
+
+    // or: wake on button press (only works once because I haven't figured out how to use rtc_gpio_deinit)
     // lowpower.deepSleepExt0(button,0);
   }, null);
 }, null);
--- src/main.c
+++ src/main.c
@@ -14,28 +14,24 @@ void esp_deep_sleep_ext0(int pin, int level) {
 }
 
 void touchpad_init(){
-	touch_pad_init();
+  touch_pad_init();
 }
 
 int print_touchpad_value(int pin){
   uint16_t val=0;
-  touch_pad_read(TOUCH_PAD_NUM0, &val); // gpio4, make this configurable
+  touch_pad_read(pin, &val);
   return val;
 }
 
 void esp_deep_sleep_touchpad(int pin){
-  touch_pad_config(TOUCH_PAD_NUM0, 700);
+  touch_pad_config(pin, 700);
   esp_deep_sleep_enable_touchpad_wakeup();
   esp_wifi_stop();
   esp_deep_sleep_start();
 }
+
+// This doesn't work ;_;
+
 // void esp_deep_sleep_init(int pin){
 //   rtc_gpio_deinit(pin);
 // }
-
-
- // * Make sure relevant WiFi and BT stack functions are called to close any
- // * connections and deinitialize the peripherals. These include:
- // *     - esp_bluedroid_disable
- // *     - esp_bt_controller_disable
- // *     - esp_wifi_stop
